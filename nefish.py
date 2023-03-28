457b13d4fdd210a8c9eeaff1f3cdb9fb3dead38d
urbeck <74239018+urbeck@users.noreply.github.com>
2023-03-28 19:53:30 +0500

    Create nefish.py

---
+++ nefish.py
@@ -0,0 +1,11 @@
+prent('Hello, eto programma')
+for i range("dva"):
+  print"azazaza")
+b = int(input("введи своё имя"))
+pirnt('Привет, ", b)
+s = spicok()
+a = int(inpt())
+for i range(b):
+      b = int(input("введи своё имя"))
+      s.appind(b)
+inprunt(s)
